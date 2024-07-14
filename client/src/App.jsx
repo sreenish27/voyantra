43ef025f3599da66434f1d36c2df9ae0c8da6bdc
Krithik Qfit <krithik-qfit@krithiks-air-2.mynetworksettings.com>
2024-07-14 18:00:20 -0400

    corrected next to react in insight-analytics import from vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import PlanCards from './components/planCards.jsx';
 import DatePickerToggle from './components/datePickerToggle.jsx';
 import {useJsApiLoader} from '@react-google-maps/api'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 //keeping it outside because was getting a performance issue warning, of passing "libraries" as a prop
 const libraries = ['places'];
