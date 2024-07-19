630bf82b543910c98075d107ffd57df9941a4ad4
Krithik Qfit <krithik-qfit@krithiks-air-2.mynetworksettings.com>
2024-07-19 17:59:43 -0400

    still testing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store.js';
 import App from './App.jsx';
-import Signuporlogin from './signupLogin.jsx';
+import Signuporlogin from './Signuporlogin.jsx';
 import './index.css';
 
 //creating my client side routing
