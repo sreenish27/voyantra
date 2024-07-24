8bdaaabbc814f84e3e618f90860097979244309c
sreenish27 <118478292+sreenish27@users.noreply.github.com>
2024-07-24 10:17:04 -0700

    reverting back a change to check if it works

--- client/src/components/planCards.jsx
+++ client/src/components/planCards.jsx
@@ -60,7 +60,7 @@ const PlanCards = () => {
         useEffect(() => {
             if((isTripDataFound === 0 || isTripDataFound === 2) && isSearchClicked){
 
-                const tripCardUrl = `https://voyantra.onrender.com/api/testing/tripcards`;
+                const tripCardUrl = `https://${Server_url}/api/testing/tripcards`;
 
                 const planCardData = async () => {
                     try{
